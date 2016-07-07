fd0ad055191533cfdab046abf958bf6dd5d1639d
rmrhz <rmrhz@tfwno.gf>
2016-07-07 12:44:33 +0900

    Only break when keys has more than one item

--- src/Prjkt/Component/Repofuck/Repofuck.php
+++ src/Prjkt/Component/Repofuck/Repofuck.php
@@ -247,7 +247,7 @@ abstract class Repofuck
 
 		foreach($inserts as $key => $val)
 		{
-			if ( ! in_array($keys, $key) ) {
+			if ( count($keys) > 0 && ! in_array($keys, $key) ) {
 				break;
 			}
 
