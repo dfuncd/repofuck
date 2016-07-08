6500748db4fa2349ab0670c878ff556432774336
rmrhz <rmrhz@tfwno.gf>
2016-07-08 12:31:30 +0900

    Fix resolving first entity

--- src/Prjkt/Component/Repofuck/Repofuck.php
+++ src/Prjkt/Component/Repofuck/Repofuck.php
@@ -125,7 +125,7 @@ abstract class Repofuck
 
 			return array_key_exists($entityName, $this->entities) ?
 				$this->entities[$entityName]:
-				$this->entities[0];
+				array_values($this->entities)[0];
 		}
 
 		if ( array_key_exists($entity, $this->entities) ) {
