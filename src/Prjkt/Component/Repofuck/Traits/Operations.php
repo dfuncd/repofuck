e57b837b4f27f89762c41aa821d2a11a9af61b85
rmrhz <rmrhz@tfwno.gf>
2016-07-24 11:55:01 +0800

    Transfered operations from Repofuck

--- src/Prjkt/Component/Repofuck/Traits/Operations.php
+++ src/Prjkt/Component/Repofuck/Traits/Operations.php
@@ -14,4 +14,25 @@ trait Operations
 	{
 		return count($array) > 0 ? true : false;
 	}
+
+	/**
+	 * Checks if the value given is an instance of Repofuck
+	 *
+	 * @param mixed
+	 * @return bool
+	 */
+	public function isRepofuck($value) : bool
+	{
+		return is_object($value) && $value instanceof \Prjkt\Component\Repofuck\Repofuck ? true : false;
+	}
+
+	/**
+	 * Resolves the name of the repository
+	 *
+	 * @return string
+	 */
+	public function resolveRepoName(\Prjkt\Component\Repofuck\Repofuck $repository) : string
+	{
+		return strtolower(str_replace('Repository', '', (new \ReflectionClass($repository))->getShortName()));
+	}
 }
