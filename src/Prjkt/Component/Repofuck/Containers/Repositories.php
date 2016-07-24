18581c3c40474f2b935093cfcf96200d18a192ac
rmrhz <rmrhz@tfwno.gf>
2016-07-24 11:46:58 +0800

    Add Repositories container

---
+++ src/Prjkt/Component/Repofuck/Containers/Repositories.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Prjkt\Component\Repofuck\Containers\Repositories;
+
+
+use Prjkt\Component\Repofuck\{
+	Exceptions\ResourceNotDefined,
+	Traits\Operations
+};
+
+class Repositories
+{
+	use Operations;
+
+	/**
+	 * Repository pointer
+	 *
+	 * @var \Prjkt\Component\Repofuck\Repofuck
+	 */
+	protected $repository;
+
+	/**
+	 * Repositories array container
+	 *
+	 * @var array
+	 */
+	protected $repositories = [];
+
+	/**
+	 * Returns the persisted repository
+	 *
+	 * @return \Prjkt\Component\Repofuck\Repofuck
+	 */
+	public function current() : \Prjkt\Component\Repofuck\Repofuck
+	{
+		return $this->repository;
+	}
+
+	/**
+	 * Pushes the repository to the container
+	 *
+	 * @param \Prjkt\Component\Repofuck\Repofuck $repository
+	 * @return \Prjkt\Component\Repofuck\Containers\Repositories
+	 */
+	public function push(\Prjkt\Component\Repofuck\Repofuck $repository) : \Prjkt\Component\Repofuck\Containers\Repositories
+	{
+		$this->repositories[$this->resolveRepoName($repository)] = $repository;
+
+		return $this;
+	}
+
+	/**
+	 * Resolves the entity given by its name
+	 *
+	 * @param string $repository
+	 * @throws \Prjkt\Component\Repofuck\Exceptions\RepositoryNotDefined
+	 * @return \Prjkt\Component\Repofuck\Repofuck
+	 */
+	public function resolve(string $repository) : \Prjkt\Component\Repofuck\Repofuck
+	{
+		if ( ! array_key_exists($repository, $this->repositories) ) {
+			throw new RepositoryNotDefined;
+		}
+
+		return $this->repositories[$repository];
+	}
+
+	/**
+	 * Sets a repository pointer
+	 *
+	 * @param \Prjkt\Component\Repofuck\Repofuck $repository
+	 * @return \Prjkt\Component\Repofuck\Repofuck
+	 */
+	protected function set(\Prjkt\Component\Repofuck\Repofuck $repository) : \Prjkt\Component\Repofuck\Containers\Repositories
+	{
+		$this->repository = $repository;
+
+		return $this;
+	}
+
+	/**
+	 * Mimics the original behavior of the DI
+	 *
+	 * @return Object
+	 */
+	public function __get($key)
+	{
+		return $this->resolve($key);
+	}
+}
