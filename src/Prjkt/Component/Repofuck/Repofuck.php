c18a8ece96d79abfcd1f04d591e6ab11c8a1c0fb
makoru-hikage <kurt.ma.coll@gmail.com>
2016-09-28 20:14:48 +0800

    New function added: where

--- src/Prjkt/Component/Repofuck/Repofuck.php
+++ src/Prjkt/Component/Repofuck/Repofuck.php
@@ -439,6 +439,44 @@ abstract class Repofuck
 	}
 
 	/**
+     * Sets the where clause to the current entity. 
+     *
+     * @param Closure $query
+     * @param bool $append
+     * @return \Prjkt\Component\Repofuck\Repofuck
+     */
+	public function where(Closure $query, $append = false)
+	{
+		$entity = $this->entity;
+
+		// Overwrite: Strip down the Builder to a Model when 
+		// the current entity has a where clause and $append is false 
+		if ($entity instanceof Builder && false === $append)
+		{
+			$entity = $this->entity->getModel();
+		}
+
+		// Supplied callback return to be attached
+		try	{
+			$entity = call_user_func($query, $entity);
+
+			if (!($entity instanceof Builder))
+			{
+				throw new InvalidCallbackReturn;
+			}
+
+		} catch (InvalidCallbackReturn $icr){
+			//Retain the current entity as it was before this method's call.
+			$entity = $this->entity;
+		}
+
+		// Assign the entity with the newly attached where clause
+		$this->entity = $entity
+
+		return $this;
+	}
+
+	/**
      * Dynamically access container services.
      *
      * @param  string  $key
